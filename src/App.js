7caea49e9e22ea5e94f999cfe574bf1340e851dd
felipey2010 <mahamaphilip@yahoo.com>
2021-02-26 18:57:44 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import logo from "./images/logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="App-logo">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      {/* <div className="App-button">
+        <button>Ver o Progresso</button>
+      </div> */}
+    </div>
+  );
 }
 
 export default App;
